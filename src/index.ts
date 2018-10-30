e92308515e904e6eed7ffe80e4d94c5a233de79f
chayakornwc <whitecat.chayakorn@gmail.com>
2018-10-30 23:29:09 +0700

    change package

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {ApiArtlampangApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {ApiArtlampangApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new ApiArtlampangApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
